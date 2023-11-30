ce4052c75d37c9e61ad6df70d734f6cd54b6a18e
asrvd <asheeshh@duck.com>
2023-11-30 20:26:36 +0530

    feat: create task for running solution of each day

--- setup.ts
+++ setup.ts
@@ -11,5 +11,16 @@ await Deno.mkdir(`./${day}`, { recursive: true });
 await Deno.create(`./${day}/input.txt`);
 await Deno.create(`./${day}/solution.ts`);
 
-console.log(`Setup for day ${day} complete!`);
+// add a new task in deno.json for the new day
+
+const denoJson = await Deno.readTextFile("./deno.json");
+const tasks = JSON.parse(denoJson).tasks;
+
+tasks[`day${day}`] = `deno run --allow-all --unstable ./${day}/solution.ts`;
+
+await Deno.writeTextFile("./deno.json", JSON.stringify({ tasks }, null, 2));
+
+console.log(
+  `Setup for day ${day} complete!\nRun 'deno task day${day}' to run the solution.`
+);
 Deno.exit(0);
