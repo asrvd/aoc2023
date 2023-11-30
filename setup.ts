500c8cb68d3b7f143fb7210a2bee91645e20d351
asrvd <asheeshh@duck.com>
2023-11-30 20:15:18 +0530

    feat: init

---
+++ setup.ts
@@ -0,0 +1,13 @@
+if (Deno.args.length !== 1) {
+  console.error("Please provide a day number.");
+  Deno.exit(1);
+}
+
+const day = Deno.args[0].padStart(2, "0");
+
+await Deno.mkdir(`./${day}`, { recursive: true });
+await Deno.create(`./${day}/input.txt`);
+await Deno.create(`./${day}/solution.ts`);
+
+console.log(`Setup for day ${day} complete!`);
+Deno.exit(0);
