8b671057e750d183f82de782be9cd0b616a59ee7
asrvd <asheeshh@duck.com>
2023-11-30 20:30:27 +0530

    feat: create tasks for running solutions

--- setup.ts
+++ setup.ts
@@ -9,18 +9,20 @@ const day = Deno.args[0].padStart(2, "0");
 
 await Deno.mkdir(`./${day}`, { recursive: true });
 await Deno.create(`./${day}/input.txt`);
-await Deno.create(`./${day}/solution.ts`);
+await Deno.create(`./${day}/s1.ts`);
+await Deno.create(`./${day}/s2.ts`);
 
-// add a new task in deno.json for the new day
+// add/edit tasks in deno.json for running solutions
 
 const denoJson = await Deno.readTextFile("./deno.json");
 const tasks = JSON.parse(denoJson).tasks;
 
-tasks[`day${day}`] = `deno run --allow-all --unstable ./${day}/solution.ts`;
+tasks[`s1`] = `deno run --allow-all --unstable ./${day}/s1.ts`;
+tasks[`s2`] = `deno run --allow-all --unstable ./${day}/s2.ts`;
 
 await Deno.writeTextFile("./deno.json", JSON.stringify({ tasks }, null, 2));
 
 console.log(
-  `Setup for day ${day} complete!\nRun 'deno task day${day}' to run the solution.`
+  `Setup for day ${day} complete!\nRun \`deno task s1\` or \`deno task s2\` to run the respective solutions for the day.`
 );
 Deno.exit(0);
